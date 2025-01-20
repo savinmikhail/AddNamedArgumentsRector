aab3696536bcac88f516824bc94d7863a54180d8
Mikhail <salazar290720035017@gmail.com>
2025-01-20 23:35:32 +0700

    add phpyh strategy

---
+++ src/Config/PhpyhStrategy.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SavinMikhail\AddNamedArgumentsRector\Config;
+
+use PhpParser\Node;
+
+use function count;
+
+final readonly class PhpyhStrategy implements ConfigStrategy
+{
+    public static function shouldApply(Node $node, array $parameters): bool
+    {
+        // Skip if there's only 1 argument
+        if (count($parameters) === 1) {
+            return false;
+        }
+
+        // Skip if there are 2 arguments with different types
+        if (
+            count($parameters) === 2
+            && !$parameters[0]->getType()->equals($parameters[1]->getType())
+        ) {
+            return false;
+        }
+
+        // Skip if there's a @no-named-argument annotation
+        $docComment = $node->getDocComment();
+        if ($docComment && str_contains($docComment->getText(), '@no-named-argument')) {
+            return false;
+        }
+
+        return true;
+    }
+}
