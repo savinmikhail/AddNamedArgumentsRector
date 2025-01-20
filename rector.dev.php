fcb57516ace8506e6ad3218edcb831b0e625812b
Mikhail <salazar290720035017@gmail.com>
2025-01-20 23:36:01 +0700

    add dev rector conf

---
+++ rector.dev.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\Class_\StringableForToStringRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use SavinMikhail\AddNamedArgumentsRector\AddNamedArgumentsRector;
+use SavinMikhail\AddNamedArgumentsRector\Config\PhpyhStrategy;
+
+return RectorConfig::configure()
+->withPaths([
+    __DIR__ . '/.task'
+    ])
+    ->withConfiguredRule(
+        AddNamedArgumentsRector::class,
+        [PhpyhStrategy::class]
+    );
