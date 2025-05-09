a4767c80456bb92469a994e556d19f2e9eaa28de
Mikhail <salazar290720035017@gmail.com>
2025-05-09 12:13:38 +0300

    refactoring strategy

--- src/Config/DefaultStrategy.php
+++ src/Config/DefaultStrategy.php
@@ -9,7 +9,10 @@ use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\New_;
 use PhpParser\Node\Expr\StaticCall;
+use PHPStan\BetterReflection\Reflection\Adapter\ReflectionClass;
+use PHPStan\BetterReflection\Reflection\Adapter\ReflectionEnum;
 use PHPStan\Reflection\ClassReflection;
+use ReflectionFunctionAbstract;
 use SavinMikhail\AddNamedArgumentsRector\Reflection\ReflectionService;
 
 final readonly class DefaultStrategy implements ConfigStrategy
@@ -19,7 +22,44 @@ final readonly class DefaultStrategy implements ConfigStrategy
         array $parameters,
         ?ClassReflection $classReflection = null,
     ): bool {
-        foreach ($node->args as $index => $arg) {
+        if (!self::areArgumentsSuitable($node->args, $parameters)) {
+            return false;
+        }
+
+        if ($classReflection !== null && !self::classAllowsNamedArguments($classReflection)) {
+            return false;
+        }
+
+        if (!self::functionAllowsNamedArguments($node, $classReflection)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static function classAllowsNamedArguments(ClassReflection $classReflection): bool
+    {
+        $reflectionClass = $classReflection->getNativeReflection();
+
+        while ($reflectionClass) {
+            if (self::hasNoNamedArgumentsTag($reflectionClass)) {
+                return false;
+            }
+            // Check if the class has @no-named-arguments annotation, even in the parent classes
+            $reflectionClass = $reflectionClass->getParentClass();
+        }
+
+        if ($classReflection->isInterface()) {
+            // 🚨 Stop rule, cuz in runtime might be resolved any implementation of the interface, and the names of arguments might differ
+            return false;
+        }
+
+        return true;
+    }
+
+    private static function areArgumentsSuitable(array $args, array $parameters): bool
+    {
+        foreach ($args as $index => $arg) {
             if (!isset($parameters[$index])) {
                 return false;
             }
@@ -29,46 +69,42 @@ final readonly class DefaultStrategy implements ConfigStrategy
                 return false;
             }
 
+            if ($arg instanceof Node\VariadicPlaceholder) {
+                return false;
+            }
+
             // Skip unpacking arguments (...$var)
             if ($arg instanceof Node\Arg && $arg->unpack) {
                 return false;
             }
 
-            if ($arg instanceof Node\VariadicPlaceholder) {
-                return false;
-            }
-
+            // skip already named arguments
             if ($arg->name !== null) {
                 return false;
             }
         }
 
-        // Check if the class has @no-named-arguments annotation, even in parent classes
-        if ($classReflection !== null) {
-            $reflectionClass = $classReflection->getNativeReflection();
+        return true;
+    }
 
-            while ($reflectionClass) {
-                $docComment = $reflectionClass->getDocComment();
-                if ($docComment !== false && str_contains($docComment, '@no-named-arguments')) {
-                    return false;
-                }
-                $reflectionClass = $reflectionClass->getParentClass();
-            }
-        }
+    private static function hasNoNamedArgumentsTag(ReflectionFunctionAbstract|ReflectionClass|ReflectionEnum $reflection): bool
+    {
+        $docComment = $reflection->getDocComment();
 
-        // Check if the function/method being called has @no-named-arguments annotation
+        return $docComment !== false && str_contains($docComment, '@no-named-arguments');
+    }
+
+    private static function functionAllowsNamedArguments(
+        FuncCall|StaticCall|MethodCall|New_ $node,
+        ?ClassReflection $classReflection = null,
+    ): bool {
         $functionReflection = ReflectionService::getFunctionReflection($node, $classReflection);
         if ($functionReflection === null) {
             return false; // 🚨 Stop rule if method doesn't exist (likely a @method annotation)
         }
 
-        $docComment = $functionReflection->getDocComment();
-        if ($docComment !== false && str_contains($docComment, '@no-named-arguments')) {
+        if (self::hasNoNamedArgumentsTag($functionReflection)) {
             return false;
-        }
-
-        if ($classReflection !== null && $classReflection->isInterface()) {
-            return false; // 🚨 Stop rule, cuz in runtime might be resolved any implementation of the interface, and the names of arguments might differ
         }
 
         return true;
