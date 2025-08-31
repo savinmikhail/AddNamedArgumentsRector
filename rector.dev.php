4e67f09b33674de177bf57c452604ebd2b084800
Savin Mikhail <mikhail.d.savin@gmail.com>
2025-08-31 08:17:01 +0300

    refactor rector.dev.php

--- rector.dev.php
+++ rector.dev.php
@@ -3,15 +3,11 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php80\Rector\Class_\StringableForToStringRector;
-use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 use SavinMikhail\AddNamedArgumentsRector\AddNamedArgumentsRector;
-use SavinMikhail\AddNamedArgumentsRector\Config\PhpyhStrategy;
 
 return RectorConfig::configure()
-->withPaths([
-//    __DIR__ . '/.task',
-    __DIR__ . '/src/AddNamedArgumentsRector.php',
+    ->withPaths([
+        __DIR__ . '/src/AddNamedArgumentsRector.php',
     ])
     ->withRules([
         AddNamedArgumentsRector::class
